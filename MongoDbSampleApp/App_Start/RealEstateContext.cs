fe805d76c987e376aea2feb72791c8df659299c5
Vaibhav Agarwal <vaibhav@parcelhero.in>
2019-01-04 19:41:32 +0530

    Mongo NET Initial Setup

---
+++ MongoDbSampleApp/App_Start/RealEstateContext.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Driver;
+using MongoDbSampleApp.Properties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MongoDbSampleApp.App_Start
+{
+    public class RealEstateContext
+    {
+        public MongoDatabase Database;
+        public RealEstateContext()
+        {
+            var client = new MongoClient(Settings.Default.RealEstateConnectionString);
+            var server = client.GetServer();
+            Database = server.GetDatabase(Settings.Default.RealEstateDatabase);
+        }
+    }
+}
--- MongoDbSampleApp/Controllers/HomeController.cs
+++ MongoDbSampleApp/Controllers/HomeController.cs
@@ -3,14 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MongoDB.Driver;
+using MongoDbSampleApp.Properties;
+using MongoDbSampleApp.App_Start;
 
 namespace MongoDbSampleApp.Controllers
 {
     public class HomeController : Controller
     {
+        public RealEstateContext context;
+        public HomeController()
+        {
+            context = new RealEstateContext();
+        }
         public ActionResult Index()
         {
-            return View();
+            //return View();
+            context.Database.GetStats();
+            return Json(context.Database.Server, JsonRequestBehavior.AllowGet);
         }
 
         public ActionResult About()
