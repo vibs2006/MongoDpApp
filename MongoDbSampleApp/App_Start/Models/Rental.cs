589db79a53b733b5205e2696916737715e25dd3a
Vibs <vibs2006@gmail.com>
2019-01-07 09:31:37 +0530

    sample unit tests and rental class

---
+++ MongoDbSampleApp/App_Start/Models/Rental.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MongoDbSampleApp.App_Start.Models
+{
+    public class Rental
+    {
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string Description { get; set; }
+        public int NumberOfRooms { get; set; }
+        public List<string> Address { get; set; } = new List<string>();
+
+        [BsonRepresentation(BsonType.Double)]
+        public decimal Price { get; set; }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MongoDbSampleApp.App_Start;
+using MongoDbSampleApp.App_Start.Models;
+using MongoDB.Bson;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ToDocument_RentalWithAnId_IdIsRepresentedAsAnObjectID()
+        {
+            var rental = new Rental();
+            rental.Id = ObjectId.GenerateNewId().ToString();
+
+            var bsonDocument = rental.ToBsonDocument();
+
+            Assert.AreEqual(bsonDocument["_id"].BsonType, BsonType.ObjectId);
+        }
+    }
+}
